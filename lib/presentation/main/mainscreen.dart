e077b20c13784c814a1b6f317bf376923233168a
Angelina <angelforworkpay@gmail.com>
2024-03-18 21:46:52 +0300

    feat: add appBar

--- lib/presentation/main/mainscreen.dart
+++ lib/presentation/main/mainscreen.dart
@@ -14,9 +14,11 @@ class _MainScreenState extends State<MainScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.yellow,
         title: Padding(
           padding: EdgeInsets.fromLTRB(8, 16, 8, 16),
           child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               const Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
@@ -28,14 +30,56 @@ class _MainScreenState extends State<MainScreen> {
                       fontWeight: FontWeight.w400,
                     ),
                   ),
-                  ListTile(
-                    leading: Icon(Icons.star),
-                    title: Text('Заголовок'),
-                    subtitle: Text('Подзаголовок'),
+                  Row(
+                    children: [
+                      Text(
+                        'Проход 456а',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      Icon(
+                        Icons.chevron_right_sharp,
+                        color: Color.fromRGBO(74, 114, 255, 1.0),
+                      ),
+                    ],
                   ),
                 ],
               ),
-              SvgPicture.asset('assets/Icon.svg'),
+              Stack(
+                // fit: StackFit.expand,
+                //textDirection: TextDirection.rtl,
+                //alignment: AlignmentDirectional.centerStart,
+                children: [
+                  SizedBox(
+                    height: 35,
+                  ),
+                  Positioned(
+                    bottom: 0,
+                    child: SvgPicture.asset(
+                      'assets/Icon.svg',
+                      width: 24,
+                      height: 24,
+                    ),
+                  ),
+                  Positioned(
+                    child: Container(
+                      width: 20,
+                      height: 20,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Color.fromRGBO(255, 87, 77, 1),
+                      ),
+                      child: const Center(
+                        child: Text('2',
+                            style:
+                                TextStyle(fontSize: 12, color: Colors.white)),
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ],
           ),
         ),
